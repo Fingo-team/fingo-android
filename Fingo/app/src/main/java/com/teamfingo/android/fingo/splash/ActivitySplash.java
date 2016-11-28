505042e5c6aee63549fe6a1276d46f2ed1dce08e
TED\taewon <kimtx200@gmail.com>
2016-11-28 18:38:55 +0900

    Make packages

---
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/splash/ActivitySplash.java
@@ -0,0 +1,15 @@
+package com.teamfingo.android.fingo.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.teamfingo.android.fingo.R;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
