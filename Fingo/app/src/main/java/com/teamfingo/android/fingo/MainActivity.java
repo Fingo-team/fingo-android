b69b1ae13589c49414ca25f2988e70c69fb6f46a
Jaemin <jaemin0216@gmail.com>
2016-11-28 17:58:21 +0900

    Initial commit

---
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamfingo.android.fingo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
