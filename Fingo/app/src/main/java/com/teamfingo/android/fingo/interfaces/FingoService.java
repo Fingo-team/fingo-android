f537094455e4638ce6fa970ff13c8eca2b617395
Jaemin <jaemin0216@gmail.com>
2016-12-01 23:57:40 +0900

    Add Call<Movie>

--- Fingo/app/src/main/java/com/teamfingo/android/fingo/interfaces/FingoService.java
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/interfaces/FingoService.java
@@ -1,10 +1,12 @@
 package com.teamfingo.android.fingo.interfaces;
 
 import com.teamfingo.android.fingo.model.BoxOfficeRanking;
+import com.teamfingo.android.fingo.model.Movie;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 
 /**
  * Created by Jaemin on 2016. 11. 29..
@@ -15,8 +17,12 @@ import retrofit2.http.Header;
 
 public interface FingoService {
 
+
     @GET("/api/v1.0/movie/boxoffice/")
     Call<BoxOfficeRanking> getBoxOfficeRanking(@Header("Authorization") String authorization);
+
+    @GET("/api/v1.0/movie/detail/{id}/")
+    Call<Movie> getMovie(@Header("Authorization") String authorization, @Path("id") String id);
 
     // SignUp function
 
