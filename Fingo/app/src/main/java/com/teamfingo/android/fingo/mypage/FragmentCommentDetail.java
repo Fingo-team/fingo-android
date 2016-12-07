cf09614fdfce0de34c2173d2a07ca38920074bf1
TED\taewon <kimtx200@gmail.com>
2016-12-07 21:22:15 +0900

    Add comment detail page

---
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/mypage/FragmentCommentDetail.java
@@ -0,0 +1,34 @@
+package com.teamfingo.android.fingo.mypage;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamfingo.android.fingo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentCommentDetail extends Fragment {
+
+
+    public FragmentCommentDetail() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_comment_detail, container, false);
+
+
+
+        return view;
+    }
+
+}
