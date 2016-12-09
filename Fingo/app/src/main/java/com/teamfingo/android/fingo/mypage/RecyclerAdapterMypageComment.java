84b9474cf33c6358fd4915c44edcf845175c8881
TED\taewon <kimtx200@gmail.com>
2016-12-09 15:06:31 +0900

    Change resource files

---
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/mypage/RecyclerAdapterMypageComment.java
@@ -0,0 +1,31 @@
+package com.teamfingo.android.fingo.mypage;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by taewon on 2016-12-09.
+ */
+
+public class RecyclerAdapterMypageComment extends RecyclerView.Adapter<RecyclerAdapterMypageComment.ViewHolder>{
+
+    public class ViewHolder {
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
