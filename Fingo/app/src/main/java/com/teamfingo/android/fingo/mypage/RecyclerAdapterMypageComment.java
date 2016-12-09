405443c5c79b67dd6fe89d75db90dc903144373b
TED\taewon <kimtx200@gmail.com>
2016-12-09 15:37:43 +0900

    Create my page comment adapter

--- Fingo/app/src/main/java/com/teamfingo/android/fingo/mypage/RecyclerAdapterMypageComment.java
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/mypage/RecyclerAdapterMypageComment.java
@@ -4,6 +4,15 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.teamfingo.android.fingo.R;
+import com.teamfingo.android.fingo.model.UserComments;
+
+import java.util.ArrayList;
 
 /**
  * Created by taewon on 2016-12-09.
@@ -12,14 +21,34 @@ import android.view.ViewGroup;
 public class RecyclerAdapterMypageComment extends RecyclerView.Adapter<RecyclerAdapterMypageComment.ViewHolder>{
 
     Context mContext;
-//    AraayList<>
+    ArrayList<UserComments> mUserComments = new ArrayList<>();
+
+    public RecyclerAdapterMypageComment(Context mContext, ArrayList<UserComments> mUserComments) {
+        this.mContext = mContext;
+        this.mUserComments = mUserComments;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgUserProfile, imgMovieStilcut;
+        TextView tvUserName, tvMovieTitle, tvComment, tvCommentDate;
+        RatingBar rbUserScore;
+        Button btnModify, btnLike, btnAddComment, btnShare;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-
-
+            imgUserProfile = (ImageView) itemView.findViewById(R.id.image_comment_profile);
+            tvUserName = (TextView) itemView.findViewById(R.id.textView_user_nickname);
+            rbUserScore = (RatingBar) itemView.findViewById(R.id.ratingBar_comment);
+            tvCommentDate = (TextView) itemView.findViewById(R.id.textView_comment_date);
+            btnModify = (Button) itemView.findViewById(R.id.button_modify);
+            imgMovieStilcut = (ImageView) itemView.findViewById(R.id.image_mypage_stilcut);
+            tvMovieTitle = (TextView) itemView.findViewById(R.id.textView_movie_title);
+            tvComment = (TextView) itemView.findViewById(R.id.textView_comment);
+            btnLike = (Button) itemView.findViewById(R.id.button_like);
+            btnAddComment = (Button) itemView.findViewById(R.id.button_comment);
+            btnShare = (Button) itemView.findViewById(R.id.button_share);
         }
     }
 
