1456c2922b7e2dfd8d2c8df9ba442ee38cd33681
Jaemin <jaemin0216@gmail.com>
2016-12-11 18:58:44 +0900

    Create AppController class
    - Application 클래스를 extends하여 만든 AppController 클래스
    - Base class for maintaining global application state.
    - The Application class, or your subclass of the Application class, is instantiated before any other class when the process for your application/package is created.
    - 자주 사용하는 Retrofit과 FingoPreference를 초기화해두고 필요한 곳에 사용

---
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/utils/AppController.java
@@ -0,0 +1,47 @@
+package com.teamfingo.android.fingo.utils;
+
+import android.app.Application;
+
+import com.teamfingo.android.fingo.interfaces.FingoService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Jaemin on 2016. 12. 9..
+ */
+
+public class AppController extends Application { // 앱이 실행되면 무조건 초기화 됨
+
+    private static String FINGO_BASE_URL = "http://fingo-dev.ap-northeast-2.elasticbeanstalk.com/";
+
+    private static Retrofit mRetrofit;
+    private static FingoPreferences mFingoPreferences;
+    private static String mToken;
+    static FingoService mService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(FINGO_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        mFingoPreferences = new FingoPreferences(this);
+        mToken = mFingoPreferences.getAccessToken();
+
+        mService = mRetrofit.create(FingoService.class);
+
+    }
+
+    public static FingoService getFingoService() {
+        return mService;
+    }
+
+    public static String getToken() {
+        return mToken;
+    }
+
+}
