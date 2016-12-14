b2bc6b20ba4c6ae9311001828e8e95c36ce99733
Jaemin <jaemin0216@gmail.com>
2016-12-15 03:44:28 +0900

    Make basic view in recommend fragment and add Statistics model class
    - Recommend 화면에 유저의 취향 통계를 보여주고 영화 평가 늘리기 기능을 넣어주기위한 기본 화면 구성
    - 취향 통계에 쓰일 Statistics model class 추가

--- Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentRecommend.java
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentRecommend.java
@@ -2,7 +2,11 @@ package com.teamfingo.android.fingo.recommend;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,7 @@ import com.teamfingo.android.fingo.R;
  */
 public class FragmentRecommend extends Fragment {
 
+    FragmentIncreaseMovieRating mFragmentIncreaseMovieRating;
 
     public FragmentRecommend() {
         // Required empty public constructor
@@ -23,8 +28,46 @@ public class FragmentRecommend extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommand, container, false);
+        View view = inflater.inflate(R.layout.fragment_recommand, container, false);
+
+        mFragmentIncreaseMovieRating = new FragmentIncreaseMovieRating();
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout_recommend);
+        tabLayout.addTab(tabLayout.newTab().setText("취향 통계"));
+        tabLayout.addTab(tabLayout.newTab().setText("영화 평가 늘리기"));
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager_recommend);
+        PagerAdapter pagerAdapter = new PagerAdapter(getFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        return view;
     }
 
+    class PagerAdapter extends FragmentStatePagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+
+            switch (position) {
+                case 0:
+                    break;
+                case 1:
+                    fragment = mFragmentIncreaseMovieRating;
+                    break;
+            }
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 }
