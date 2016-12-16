e88dad26dbae6d86ce57299e354536e02ffe5cce
kimtx200 <kimtx200@gmail.com>
2016-12-17 01:52:08 +0900

    Correct fragment name

--- Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentIncreaseMovieRating.java
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentIncreaseMovieRating.java
@@ -24,7 +24,7 @@ public class FragmentIncreaseMovieRating extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_increase_movie_rating, container, false);
+        return inflater.inflate(R.layout.fragment_increase_movie_rating, container, false);
     }
 
 }
