94da0e3d2382bca08f668c5bbedd1a9f5b8dc147
Jaemin <jaemin0216@gmail.com>
2016-12-17 17:04:12 +0900

    Change name

--- Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentRandomMovie.java
+++ Fingo/app/src/main/java/com/teamfingo/android/fingo/recommend/FragmentRandomMovie.java
@@ -3,6 +3,7 @@ package com.teamfingo.android.fingo.recommend;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,11 @@ import com.teamfingo.android.fingo.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentIncreaseMovieRating extends Fragment {
+public class FragmentRandomMovie extends Fragment {
 
+    RecyclerView mRecyclerView;
 
-    public FragmentIncreaseMovieRating() {
+    public FragmentRandomMovie() {
         // Required empty public constructor
     }
 
@@ -23,8 +25,10 @@ public class FragmentIncreaseMovieRating extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_increase_movie_rating, container, false);
+        View view = inflater.inflate(R.layout.fragment_increase_movie_rating, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_increase_movie_rating);
+        return view;
     }
 
 }
